dbcb891b2c3c33e77218321066307a479a52d980
krgiani <kr.giani@gmail.com>
2017-05-15 21:23:50 +1000

    randomize initial colours

--- colorGame.js
+++ colorGame.js
@@ -1,16 +1,10 @@
-var colors = [
-	"rgb(255, 0, 0)",
-	"rgb(255, 255, 0",
-	"rgb(0, 255, 0)",
-	"rgb(0, 255, 255)",
-	"rgb(0, 0, 255)",
-	"rgb(255, 0, 255)"
-];
+var colors = generateRandomColors(6);
 
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
+var h1 = document.querySelector("h1");
 
 colorDisplay.textContent = pickedColor;
 
@@ -28,6 +22,7 @@ for (var i = 0; i < squares.length; i++) {
 		if(clickedColor === pickedColor) {
 			messageDisplay.textContent = "Correct!";
 			changeColors(clickedColor);
+			h1.style.background = clickedColor;
 		}
 		else {
 			this.style.background = "#232323";
@@ -48,4 +43,32 @@ function changeColors(color) {
 function pickColor() {
 	var random = Math.floor(Math.random() * colors.length);
 	return colors[random];
+}
+
+function generateRandomColors(num) {
+	// make an array
+	var arr = [];
+
+	// add num random colors to arr
+	for (var i = 0; i < num; i++) {
+
+		// get random color and push into arr
+		arr.push(randomColor());
+	}
+
+	return arr;
+}
+
+function randomColor() {
+
+	//pick a "red" from 0-255
+	var r = Math.floor(Math.random() * 256);
+
+	//pick a "green" from 0-255
+	var g = Math.floor(Math.random() * 256);
+
+	//pick a "blue" from 0-255
+	var b = Math.floor(Math.random() * 256);
+
+	return "rgb(" + r + ", " + g + ", " + b + ")";
 }
